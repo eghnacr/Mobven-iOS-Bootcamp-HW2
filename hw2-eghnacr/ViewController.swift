02eee6671182b19c6803a0ccfcf35781daf66db1
eghnacr <eghnacr@gmail.com>
2021-12-21 15:39:25 +0300

    Initial Commit

---
+++ hw2-eghnacr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw2-eghnacr
+//
+//  Created by Egehan Acar on 21.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
